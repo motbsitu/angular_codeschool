6dc19d1eddb87bd921c4be4ba179afe388062612
Julie S Mike <juliesmike@gmail.com>
2017-01-16 18:23:39 -0600

    Directives, controllers, modules, expressions

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-var app = angular.module('store', []);
+(function(){
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  var gems = [
+  {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'Bacon ipsum dolor amet andouille turducken flank hamburger tenderloin jerky.',
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name: 'Pentagonal Gem',
+    price: 5.95,
+    description: 'Meatloaf pork ball tip andouille strip steak picanha boudin, hamburger tenderloin prosciutto.',
+    canPurchase: false,
+    soldOut: true
+  }
+
+];
+})();
