50d37b17c15c2bac214fd59e3824fe1f14274046
Julie S Mike <juliesmike@gmail.com>
2017-01-19 14:25:52 -0600

    validations

--- app.js
+++ app.js
@@ -10,8 +10,14 @@
  });
 
   app.controller("ReviewController", function(){
-    
-  })
+    this.review ={};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review ={};
+    };
+  });
+
   app.controller('StoreController', function(){
     this.products = gems;
   });
