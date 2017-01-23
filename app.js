cc4d556630e588b40ffc37226db5f5eb3fef5e46
Julie S Mike <juliesmike@gmail.com>
2017-01-23 16:00:49 -0600

    more directives, panels not working

--- app.js
+++ app.js
@@ -23,16 +23,7 @@
     this.products = gems;
   });
 
-  app.controller("PanelController", function(){
-    this.tab = 1;
 
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    };
-  });
 
   app.directive('productTitle', function(){
     return {
@@ -56,6 +47,39 @@
       templateUrl: "product-specs.html"
     };
   });
+
+  app.directive("productPanels", function(){
+    return{
+      restrict:'E',
+      templateUrl: 'product-panels.html',
+      controller: function(){
+          this.tab = 1;
+
+          this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+          };
+
+          this.selectTab = function(setTab){
+            this.tab = setTab;
+          };
+
+        },
+        controllerAs:'panels'
+    };
+  });
+
+    app.directive("productGallery", function(){
+      return{
+        restrict: 'E',
+        templateUrl: 'product-gallery.html',
+        controller: function(){
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+      };
+    }, controllerAs: 'gallery'
+    };
+    });
 
   var gems = [
       {
